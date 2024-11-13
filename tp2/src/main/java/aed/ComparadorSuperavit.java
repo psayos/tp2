26c403efde413556bfebf82afb3f8305d1faf8c4
psayos <33334048+psayos@users.noreply.github.com>
2024-11-13 17:21:32 -0300

    Add files via upload

---
+++ tp2/src/main/java/aed/ComparadorSuperavit.java
@@ -0,0 +1,16 @@
+package aed;
+
+import java.util.Comparator;
+
+public class ComparadorSuperavit implements Comparator<Ciudad> { //O(1)
+
+    @Override
+    public int compare(Ciudad c1, Ciudad c2) {
+        if (c1.superavit() == c2.superavit()) {
+            return Integer.compare(c2.id(), c1.id());
+        } else {
+            return Integer.compare(c1.superavit(), c2.superavit());
+        }
+    }
+}
+
